b7e00ee1aeaab474a5c5f72bf8b7ae039f868f9c
MygJae <50861514+mygjae@users.noreply.github.com>
2024-03-07 10:02:00 +0900

    절차 지향 프로그래밍

---
+++ src/oop1/MusicPlayerMain2.java
@@ -0,0 +1,45 @@
+package oop1;
+
+public class MusicPlayerMain2 {
+
+    public static void main(String[] args) {
+        /**
+         * 음악 플레이어 만들기
+         *   절차 지향 음악 플레이어2
+         *   음악 플레이어와 관련된 데이터 묶기
+         */
+
+        MusicPlayerData data = new MusicPlayerData();
+
+        // 음악 플레이어 켜기
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+
+        //볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨: " + data.volume);
+
+        //볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨: " + data.volume);
+
+        //볼륨 감소
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨: " + data.volume);
+
+        //음악 플레이어 상태
+        System.out.println("음악 플레이어 상태 확인");
+        if (data.isOn) {
+            System.out.println("data.volume = " + data.volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+
+        //음악 플레이어 끄기
+        data.isOn = false;
+        System.out.println("음악 플레이어 종료합니다.");
+    }
+
+
+}
+
